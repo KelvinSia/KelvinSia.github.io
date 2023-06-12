599ee1f16a53b695260bebcd6187080c17095dd0
KelvinSia <87654927+KelvinSia@users.noreply.github.com>
2023-06-12 17:39:04 +0800

    Added Alot

---
+++ script.js
@@ -0,0 +1,78 @@
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+window.addEventListener('beforeunload', scrollToTop);
+
+// Expandable Tab
+
+function toggleContent() {
+  var content = document.getElementById("myContent-expandable");
+  content.classList.toggle("show");
+}
+
+// Jump to Div
+
+function triggerDivClick(divId) {
+  var divElement = document.getElementById(divId);
+  divElement.focus();
+  divElement.scrollIntoView({ behavior: "smooth" });
+}
+
+// Slimess
+
+var dd = 3;
+var cnt = 0;
+
+function left() {
+
+  var rr = document.getElementById('slime-content');
+  var imageDir = 'Image/Slimess/'; 
+
+  var images = [
+    "Slime_Pew_1.jpg",
+    "Slime_Pew_2.jpg",
+    "Slime_Pew_3.jpg",
+  ];
+
+  cnt++;
+
+  var len = images.length;
+  if (cnt < dd) {
+    rr.src = imageDir + images[cnt];
+  }
+  else if (cnt == len) {
+    rr.src = imageDir + images[0];
+    cnt = 0;
+  }
+}
+
+function right() {
+
+  var rr = document.getElementById('slime-content');
+  var imageDir = 'Image/Slimess/'; 
+
+  var images = [
+    "Slime_Pew_1.jpg",
+    "Slime_Pew_2.jpg",
+    "Slime_Pew_3.jpg",
+  ];
+
+  cnt--;
+
+  var len = images.length;
+  if (cnt < dd) {
+    rr.src = imageDir + images[cnt];
+  }
+  else if (cnt == len) {
+    rr.src = imageDir + images[0];
+    cnt = 0
+  }
+
+  if (cnt < 0) {
+    rr.src = imageDir + images[2];
+    cnt = 2
+  }
+};
+
+// END
